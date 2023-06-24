dc2a156506cc4765883ed8abffb2b945f427e639
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2023-06-24 16:52:51 -0600

    Finished the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     extend: {
       colors: {
         'light-green': '#DAE8D5',
+        'button': '#A41D20'
+      },
+      height: {
+        '50': '50vh',
+      },
+      width: {
+        '50rem': '50rem',
+        '35rem': '35rem',
+      },
+      borderRadius: {
+        '2': '2rem',
       }
     },
   },
