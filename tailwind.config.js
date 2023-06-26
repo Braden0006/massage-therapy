a1795c83a1bc93c86a8ea1765577a6e0e6a4469a
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2023-06-26 10:48:04 -0600

    Finished menu and body-wrap pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       colors: {
         'light-green': '#DAE8D5',
-        'button': '#A41D20'
+        'button': '#A41D20',
+        'h2-green': '#35701F',
       },
       height: {
         '50': '50vh',
@@ -13,11 +14,15 @@ module.exports = {
       },
       width: {
         '50rem': '50rem',
+        '42rem': '42rem',
         '40vw': '40vw',
         '35rem': '35rem',
       },
       borderRadius: {
         '2': '2rem',
+      },
+      lineHeight: {
+        '6rem': '6rem',
       }
     },
   },
